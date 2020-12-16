b9d279202ffa767c039c3fcc8d3269310e4d5483
Mason Lough <70122910+NighthawkTheTechnician@users.noreply.github.com>
2020-12-16 08:33:27 -0600

    Update Nth.swift

--- Swift/N/Nth.swift
+++ Swift/N/Nth.swift
@@ -1,1 +1,31 @@
+import Foundation
 
+func insertSuffix(num: Int) -> String {
+    if num % 100 / 10 == 1 {
+        return "th"
+    }
+ 
+    switch num % 10 {
+    case 1:
+        return "st"
+    case 2:
+        return "nd"
+    case 3:
+        return "rd"
+    default:
+        return "th"
+    }
+}
+ 
+for Nth in 0...25 {
+    print("\(Nth)\(insertSuffix(num: Nth)) ")
+}
+print()
+for Nth in 250...265 {
+    print("\(Nth)\(insertSuffix(num: Nth)) ")
+}
+print()
+for Nth in 1000...1025 {
+    print("\(Nth)\(insertSuffix(num: Nth)) ")
+}
+print()
