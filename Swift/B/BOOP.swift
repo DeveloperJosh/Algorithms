30397121f777811faa48e96031bff5c1a2c44f7b
Mason Lough <70122910+NighthawkTheTechnician@users.noreply.github.com>
2020-12-17 05:17:42 -0600

    Create BOOP.swift

---
+++ Swift/B/BOOP.swift
@@ -0,0 +1,11 @@
+struct boopTest {
+    var notSoSecret = "Hello world!"
+    private var numericalSecret = 140
+}
+ 
+let boopCall = boopTest()
+let boopMirror = Mirror(reflecting: boopCall)
+ 
+if let numericalSecret = boopMirror.children.filter({ $0.label == "numericalSecret" }).first?.value {
+    print("The secret value is: \(numericalSecret)")
+}
