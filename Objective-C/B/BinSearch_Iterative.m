0a7cca3e8184ceeeeb697eab44bad3aefa653800
Synterragen <70122910+Synterragen@users.noreply.github.com>
2020-12-22 17:40:26 -0600

    Update BinSearch_Iterative.m

--- Objective-C/B/BinSearch_Iterative.m
+++ Objective-C/B/BinSearch_Iterative.m
@@ -1,1 +1,37 @@
+#import <Foundation/Foundation.h>
 
+@interface NSArray (BinarySearch)
+- (NSInteger) binSearch:(id)key;
+@end
+
+@implementation NSArray (BinarySearch)
+- (NSInteger)binSearch:(id)key {
+    NSInteger lowerIndex = 0;
+    NSInteger upperIndex = [self count] - 1;
+    while (lowerIndex <= upperIndex) {
+        NSInteger midIndex = lowerIndex + (upperIndex - lowerIndex) / 2;
+        id centVal = self[midIndex];
+        
+        switch([centVal compare:key]) {
+            case NSOrderedAscending:
+                lowerIndex = midIndex + 1;
+                break;
+            case NSOrderedDescending:
+                upperIndex = midIndex - 1;
+                break;
+            case NSOrderedSame:
+                return midIndex;
+        }
+    }
+    return NSNotFound;
+}
+@end
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        NSArray *values = @[@1, @2, @3, @4];
+        NSLog(@"Value is in array element: %li", [values binSearch:@4]);
+    }
+    return 0;
+}
